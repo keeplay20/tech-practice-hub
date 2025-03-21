fe558ae486224b9a8c39380fec0a1a0232ddb87d
Mandar Vyas <mandar.vyas@dream11.com>
2025-03-21 09:34:20 +0530

    chore: variables

---
+++ Javascript/Concepts/variables.js
@@ -0,0 +1,26 @@
+function testVarVariable() {
+  var a = 10;
+  console.log("============================\nVar Before if ", a);
+  if (true) {
+    var a = 20;
+    console.log("Var Inside if ", a);
+  }
+  console.log("Var After if", a, "\n============================");
+}
+
+function testLetVariable() {
+  let a = 10;
+  const b = 20;
+  console.log("Let Before if ", a);
+
+  if (true) {
+    let a = 20;
+
+    console.log("Let Inside if ", a);
+  }
+  console.log("Const ", b);
+  console.log("Let After if", a, "\n============================");
+}
+
+testVarVariable();
+testLetVariable();

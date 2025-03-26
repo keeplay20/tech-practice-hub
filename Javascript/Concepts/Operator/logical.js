f74ebe52c77edabecedfa65b5b3bf3d6d0ab310b
keeplay20 <mandar.vyas20@gmail.com>
2025-03-26 10:36:16 +0530

    chore: logical opr

---
+++ Javascript/Concepts/Operator/logical.js
@@ -0,0 +1,6 @@
+let age = 25;
+let hasLicense = true;
+
+console.log(age >= 18 && hasLicense); // true (AND)
+console.log(age < 18 || hasLicense); // true (OR)
+console.log(!hasLicense); // false (NOT)

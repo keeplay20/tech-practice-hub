c6c1b31339d742182e15593d8185ace1a60cdd40
keeplay20 <mandar.vyas20@gmail.com>
2025-03-26 08:37:13 +0530

    chore: opr comparison

---
+++ Javascript/Concepts/Operator/comparison.js
@@ -0,0 +1,6 @@
+console.log(5 == "5"); // true (loose equality)
+console.log(5 === "5"); // false (strict equality)
+console.log(10 > 5); // true
+console.log(10 < 5); // false
+console.log(10 >= 10); // true
+console.log(10 !== 10); // false

e607eb65497f920c1c01643bbd442673708d3afb
keeplay20 <mandar.vyas20@gmail.com>
2025-04-15 09:10:33 +0530

    chore: celcius to f

---
+++ Javascript/Interview-Prep/celToFar.js
@@ -0,0 +1,8 @@
+function calculateFahrenheit(celsius) {
+  // Convert Celsius to Fahrenheit
+  return (celsius * 9) / 5 + 32;
+}
+
+console.log(calculateFahrenheit(0));
+console.log(calculateFahrenheit(36));
+console.log(calculateFahrenheit(100));

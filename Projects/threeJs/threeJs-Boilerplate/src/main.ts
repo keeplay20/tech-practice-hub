dd9803a084a128740c5bb5a1f62b8bf7c8360f33
keeplay20 <mandar.vyas20@gmail.com>
2025-04-30 09:55:01 +0530

    chore: boilerplate complete

--- Projects/threeJs/threeJs-Boilerplate/src/main.ts
+++ Projects/threeJs/threeJs-Boilerplate/src/main.ts
@@ -1,6 +1,8 @@
 import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import Stats from "three/examples/jsm/libs/stats.module.js";
+import { GUI } from "dat.gui";
 
 const scene = new THREE.Scene();
 
@@ -16,19 +18,34 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-new OrbitControls(camera, renderer.domElement);
-
 window.addEventListener("resize", () => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
+new OrbitControls(camera, renderer.domElement);
+
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshNormalMaterial({ wireframe: true });
 
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+
+const stats = new Stats();
+document.body.appendChild(stats.dom);
+
+const gui = new GUI();
+const cubeFolder = gui.addFolder("Cube");
+cubeFolder.add(cube.rotation, "x", 0, Math.PI * 2);
+cubeFolder.add(cube.rotation, "y", 0, Math.PI * 2);
+cubeFolder.add(cube.rotation, "z", 0, Math.PI * 2);
+cubeFolder.open();
+
+const cameraFolder = gui.addFolder("Camera");
+cameraFolder.add(camera.position, "x", -10, 10);
+cameraFolder.add(camera.position, "z", 0, 20);
+cameraFolder.add(camera.position, "y", -10, 10);
 
 function animate() {
   requestAnimationFrame(animate);
@@ -37,6 +54,7 @@ function animate() {
   // cube.rotation.y += 0.01;
 
   renderer.render(scene, camera);
+  stats.update();
 }
 
 animate();

bd1dcc7f35c0e7d0d2f526dfde5bd8b1914e93f2
keeplay20 <mandar.vyas20@gmail.com>
2025-05-19 18:56:58 +0530

    chore: apple landing page

--- React/motion-playground/src/App.jsx
+++ React/motion-playground/src/App.jsx
@@ -1,12 +1,22 @@
-import "./App.css";
-import { motion } from "motion/react";
+import { Canvas } from "@react-three/fiber";
+import React from "react";
+import "./style.css";
+import { Environment, ScrollControls } from "@react-three/drei";
+import MacContainer from "./MacContainer";
 
-function App() {
+const App = () => {
   return (
-    <>
-      <motion.div></motion.div>
-    </>
+    <Canvas camera={{ fov: 12, position: [0, -10, 220] }}>
+      <Environment
+        files={[
+          "https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_09_4k.exr",
+        ]}
+      />
+      <ScrollControls pages={3}>
+        <MacContainer />
+      </ScrollControls>
+    </Canvas>
   );
-}
+};
 
 export default App;

f058e535edfe7f4c07b977a093e3ee004cc2fde0
keeplay20 <mandar.vyas20@gmail.com>
2025-10-03 22:29:07 +0530

    chore: day 9 dsa

---
+++ DSA/day9/01.js
@@ -0,0 +1,11 @@
+// Accept size n from user and create an n size array then take n inputs into it and finally print the sum & average of all elements.
+
+let size = Number(prompt("Enter size of array : "));
+let sum = 0;
+for (let i = 1; i <= size; i++) {
+  let arr = Number(prompt("Enter numbers in array : "));
+  console.log(arr);
+  sum = sum + arr;
+}
+
+console.log(`The sum of all elements is ${sum}`);

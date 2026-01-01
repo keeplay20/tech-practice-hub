ebd62edef4006b32103c67a40dd9247fe240c7e4
keeplay20 <mandar.vyas20@gmail.com>
2026-01-01 23:07:10 +0530

    chore: home page added

--- React/login-react/login/src/main.jsx
+++ React/login-react/login/src/main.jsx
@@ -11,6 +11,7 @@ createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
